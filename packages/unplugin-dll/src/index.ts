542a5b778f9088490812bc844ad0316293bf9898
Michael Ostermann <michaelostermann@me.com>
2025-12-06 14:43:59 +0100

    chore: migrations

--- packages/unplugin-dll/src/index.ts
+++ packages/unplugin-dll/src/index.ts
@@ -25,7 +25,7 @@ export default createUnplugin<Options>(({ debug, enforce, exclude, include } = {
                     debug: shouldDebug(id),
                     resolve({ importAlias, importName, importPath, propertyName }) {
                         if (importPath === "@monstermann/dll" && importName === "Dll")
-                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/dll/Dll/${propertyName}.js"`
+                            return `import { ${propertyName} as ${importAlias} } from "@monstermann/dll/Dll/${propertyName}.mjs"`
                         return undefined
                     },
                 })
